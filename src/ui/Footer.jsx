dac9cc53c0b791a732f9f610ae9a6bc9d496f600
Othmane Nissoukin <othmane2000kaido@gmail.com>
2024-02-03 10:00:08 +0100

    update logo

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -6,7 +6,7 @@ function Component() {
       <div className="max-w-7xl lg:max-w-5xl md:max-w-3xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link to={"/"} className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <img src="assets/logo.png" className="h-16" alt="Flowbite Logo" />
+            <img src="assets/logo.jpg" className="h-16" alt="Flowbite Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HT-3NDI</span>
           </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -8,8 +8,10 @@ function Footer() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-7xl lg:max-w-5xl md:max-w-3xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="assets/logo.png" className="h-8" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HT-3NDI</span>
+          <img src="assets/logo.jpg" className="h-8" alt="Flowbite Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-blue-600 dark:text-white">
+            HT-3NDI
+          </span>
         </Link>
         <button
           data-collapse-toggle="navbar-default"
